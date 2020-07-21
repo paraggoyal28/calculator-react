56efbf4224a9230f2f05d2dea2fb873136a267c6
paraggoyal28 <2015uec1209@mnit.ac.in>
2020-07-21 12:43:49 +0530

    Updates

---
+++ precache-manifest.3392be7f8192c3ef7dd37be4e09a7331.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f87a5fc1a86cc6929ef18fc05660793",
+    "url": "/calculator-react-typescript/index.html"
+  },
+  {
+    "revision": "0ff4876f8c739593aac4",
+    "url": "/calculator-react-typescript/static/css/main.93371e6c.chunk.css"
+  },
+  {
+    "revision": "30d864dd91e99c99f930",
+    "url": "/calculator-react-typescript/static/js/2.482af361.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculator-react-typescript/static/js/2.482af361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ff4876f8c739593aac4",
+    "url": "/calculator-react-typescript/static/js/main.00a9682c.chunk.js"
+  },
+  {
+    "revision": "c2366df1c70bb3678865",
+    "url": "/calculator-react-typescript/static/js/runtime-main.0e2c577e.js"
+  }
+]);
